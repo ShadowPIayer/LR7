28bddb519d1c912318b90bea85889aceafae5b15
ShadowPIayer <andgorod4kov5@gmail.com>
2021-10-28 16:33:51 +0300

    First

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float z, x, y;
+    cout << "Input X" << endl;
+    cin >> x;
+    cout << "Input Y" << endl;
+    cin >> y;
+    z = 1;
+    for (int k = 1; k < 6; k++)
+    {
+        y *= log(k*y);
+    }
+    z = pow(y,5)+y;
+    cout << "Z=" << fixed << setprecision(2) << z;
+}
